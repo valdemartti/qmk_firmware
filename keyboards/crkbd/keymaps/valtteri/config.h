578a72a75b6a06c47ffab755dda6dc75afb80ac5
Valtteri Palonkorpi <ext-valtteri.palonkorpi@elisa.fi>
2024-04-08 07:11:12 +0300

    feat: Configured split

--- keyboards/crkbd/keymaps/valtteri/config.h
+++ keyboards/crkbd/keymaps/valtteri/config.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
+#define SPLIT_USB_DETECT
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
