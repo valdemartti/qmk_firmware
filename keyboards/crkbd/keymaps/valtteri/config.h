0d4424c0ff4d9aa5f702929ee60fcd44f04e5e23
Valtteri Palonkorpi <ext-valtteri.palonkorpi@elisa.fi>
2024-04-23 08:14:48 +0300

    feat: Adjusted terms

--- keyboards/crkbd/keymaps/valtteri/config.h
+++ keyboards/crkbd/keymaps/valtteri/config.h
@@ -35,8 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define OLED_TIMEOUT 300000
 
-//#define QUICK_TAP_TERM 0
-//#define TAPPING_TERM 100
+#define TAPPING_TERM 210
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
